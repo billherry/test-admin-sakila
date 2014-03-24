405fcbd37fba93a85bd7771caa6cf0ba5ab249ac
billherry <vincze.zsolt.sandor@gmail.com>
2014-03-24 12:54:02 +0100

    Grid

--- target/WebContent/store.js
+++ target/WebContent/store.js
@@ -11,13 +11,38 @@ Ext.onReady(function(){
         }),
 		fields : [  
 		           {name : 'actor_id',type : 'int'},
-		           'first_name','last_name',{name:'last_update',type:'date'}]	
+		           'first_name','last_name',{name:'last_update',type:'date', dateformat:'c'}]	
 	});
 	  
 	store.load({
 		callback: function() {
 			console.log(store.getCount());
 		}
+	});	
+	var grid = new Ext.grid.GridPanel({
+	    store:store,
+	    colModel: new Ext.grid.ColumnModel({
+	        defaults: {
+	            width: 100,
+	            sortable: true
+	        },
+	        columns: [
+	            {id: 'actor_id', header: 'ID', width: 30, sortable: true, dataIndex: 'actor_id'},
+	            {header: 'First Name',dataIndex: 'first_name'},
+	            {header: 'Last Name', dataIndex: 'last_name'},	           
+	            {
+	                header: 'Last Updated', dataIndex: 'last_update',
+	                xtype: 'datecolumn', format: 'Y.M.d'
+	            }
+	        ]
+	    }),
+	    sm: new Ext.grid.RowSelectionModel({singleSelect:true}),
+	    width: 350,
+	    height: 300,
+	    frame: false,
+	    title: 'Actors form sakila'
 	});
+	
+	grid.render('array_grid');
 	window.store = store;
 });
