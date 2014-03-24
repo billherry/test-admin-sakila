16bbcde78cb33ce63d92db95533e18825c0b48e7
billherry <vincze.zsolt.sandor@gmail.com>
2014-03-24 12:09:13 +0100

    ActorServlet

--- src/main/java/com/lightport/sakila/servlet/ActorServlet.java
+++ src/main/java/com/lightport/sakila/servlet/ActorServlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
+import com.lightport.sakila.dataconnection.JdbcHelper;
+import com.lightport.sakila.dataconnection.JsonHelper;
+
 /**
  * Servlet implementation class ActorServlet
  */
@@ -27,14 +32,20 @@ public class ActorServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter writer = response.getWriter();
-		writer.printf("GetActorServlet");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		JdbcHelper.openConnect();
+		JsonHelper jsonHelper = new JsonHelper();
+		JSONObject jsonObject = jsonHelper .getJsonObject("select *from actor");
+		//JSONObject jsonObject2 = jsonHelper .getJsonObject("select * from actor where actor_id = 42");
+		PrintWriter writer = response.getWriter();	
+		
+		writer.printf("%s",jsonObject);		
+		writer.flush();
+		writer.close();
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		writer.printf("{images:[{'name':'aaaa', 'size':'46' ,'date':'2011.11.11'}]}");
 	}
 
 }
